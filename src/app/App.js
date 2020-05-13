aadf8ef2d8c7ba33f9ce64d32e9449101f9356dd
zalio <orulluoglu.zeynel@gmail.com>
2020-05-14 01:26:11 +0300

    app.js'e localstorage template

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './assets/styles/App.scss';
 import Content from "./Content.js";
 
-function App() {
+const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        
+    }, []);
+
   return (
     <div className="App">
       <Content />
