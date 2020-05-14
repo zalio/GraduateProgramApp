bc2221f2626e60a27ab2fe079d0e6b0656b396d2
zalio <orulluoglu.zeynel@gmail.com>
2020-05-15 02:43:23 +0300

    loading screen is added

--- src/components/reusable/LoadingScreen.js
+++ src/components/reusable/LoadingScreen.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Loader from 'react-loader-spinner'
+import './loadingScreen.scss';
 
-const LoadingScreen = () => {
-    return <>Loading Screen</>;
-};
-
-export default LoadingScreen;
+export default class LoadingScreen extends React.Component {
+    render() {
+        return(
+            <div id='loading-screen-container'>
+                <Loader
+                    type="Puff"
+                    color="green"
+                    height={130}
+                    width={130}
+                />
+            </div>
+        );
+    }
+}
