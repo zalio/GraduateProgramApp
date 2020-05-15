df949317c1ae627e3cbb91bd9d9d594960a99881
ridvanmertoglu <47368549+ridvanmertoglu@users.noreply.github.com>
2020-05-15 03:00:34 +0300

    feat: add register page

--- src/app/Content.js
+++ src/app/Content.js
@@ -3,13 +3,14 @@ import { Switch, Route, useLocation } from "react-router-dom";
 
 import Dashboard from '../pages/Dashboard';
 import Login from "../pages/Login";
+import Register from '../pages/Register';
 import NoRoute from '../pages/NoRoute';
 
 const Content = ({isSignIned}) => {
     const location = useLocation();
     return (
         <Switch location={location} key={location.pathname}>
-            <Route exact path="/" component={Dashboard} />
+            <Route exact path="/" component={Register} />
             <Route component={NoRoute} />
         </Switch>
     );
