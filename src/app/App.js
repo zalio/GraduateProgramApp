3f62a6ef063f11543051f6fe8fbae81e0cf1ae13
Burak Sak <buraksak98@gmail.com>
2020-05-15 18:03:00 +0300

    feat: add eslint and prettier, feat: format code on save

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import './assets/styles/App.scss';
+import React, { useState, useEffect } from "react";
+import "./assets/styles/App.scss";
 import Content from "./Content.js";
 import LoadingScreen from "../components/reusable/LoadingScreen";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
 const App = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [userData, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
 
-    useEffect(() => {
-        const userFromLS = localStorage.getItem('userData');
-        if (userFromLS === null) {
-            setIsLoading(false);
-        } else {
-            setUserData('token');
-            setIsLoading(false);
-        }
-    }, []);
-
-    const renderItem = () => {
-        if (isLoading) return <LoadingScreen/>;
-        else if (!userData) return <Register/>;
-        return <Content isSignIned={userData !== null} />;
+  useEffect(() => {
+    const userFromLS = localStorage.getItem("userData");
+    if (userFromLS === null) {
+      setIsLoading(false);
+    } else {
+      setUserData("token");
+      setIsLoading(false);
     }
-    return (
-        <div className="App">
-            {renderItem()}
-        </div>
-    );
+  }, []);
+
+  const renderItem = () => {
+    if (isLoading) return <LoadingScreen />;
+    else if (!userData) return <Register />;
+    return <Content isSignIned={userData !== null} />;
+  };
+  return <div className="App">{renderItem()}</div>;
 };
 
 export default App;
