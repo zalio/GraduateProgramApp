8064d5aaaccb7ad0c4d86d708674b1a820698441
ridvanmertoglu <47368549+ridvanmertoglu@users.noreply.github.com>
2020-05-17 17:58:57 +0300

    feat: add register page

--- src/app/App.js
+++ src/app/App.js
@@ -4,11 +4,10 @@ import Content from "./Content.js";
 import LoadingScreen from "../components/reusable/LoadingScreen";
 import Login from "../pages/Login";
 import Button from "@material-ui/core/Button";
-import {connect} from "react-redux";
-import Register from '../pages/Register';
+import { connect } from "react-redux";
+import Register from "../pages/Register";
 
-
-import {themeChanger} from "../store/actions/application";
+import { themeChanger } from "../store/actions/application";
 
 const App = (props) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -24,25 +23,37 @@ const App = (props) => {
     }
   }, []);
 
-  const themeButton = () =>
-      <Button
-          id="theme-button"
-          variant="contained"
-          onClick={() =>
-              props.themeChanger()}>
-        OPEN {props.mode === 'light' ? 'DARK' : 'LIGHT'} MODE
-      </Button>;
+  const themeButton = () => (
+    <Button
+      id="theme-button"
+      variant="contained"
+      onClick={() => props.themeChanger()}
+    >
+      OPEN {props.mode === "light" ? "DARK" : "LIGHT"} MODE
+    </Button>
+  );
 
   const renderItem = () => {
     if (isLoading) return <LoadingScreen />;
-    else if (!userData) return <div>{themeButton()}<Login /></div>;
-    return <div>{themeButton()}<Content isSignIned={userData !== null} /></div>;
+    else if (!userData)
+      return (
+        <div>
+          {themeButton()}
+          <Login />
+        </div>
+      );
+    return (
+      <div>
+        {themeButton()}
+        <Content isSignIned={userData !== null} />
+      </div>
+    );
   };
   return <div className="App">{renderItem()}</div>;
 };
 
-const mapStateToProps = ({applicationReducer}) => {
-  const {mode} = applicationReducer;
+const mapStateToProps = ({ applicationReducer }) => {
+  const { mode } = applicationReducer;
   return {
     mode,
   };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,107 +1,98 @@
-import React, {useState, useEffect} from 'react';
-import FormControl from '@material-ui/core/FormControl';
+import React, { useState, useEffect } from "react";
+import FormControl from "@material-ui/core/FormControl";
 import FormGroup from "@material-ui/core/FormGroup";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import './register.scss';
-import {connect} from "react-redux";
-
-
-
+import "./register.scss";
+import { connect } from "react-redux";
 
 const Register = (props) => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
-    const [surname, setSurname] = useState('');
-    const [password, setPassword] = useState('');
-    const [rePassword, setRePassword] = useState('');
+  const registerHandler = () => console.log("Kayıt olundu.");
 
-    const registerHandler = () => console.log("Kayıt olundu.");
-
-    return (
-        <div id='register-page' className={props.mode}>
-            <div id={'register-page-general'} className={props.mode}>
-                <div id={"register-container"} className={props.mode}>
-                    <FormControl noValidate autoComplete={"off"}>
-                        <FormGroup row={false}>
-                            <div id={"name-surname-container"} className={props.mode}>
-                                <div id={"register-name-container"} className={props.mode}>
-                                    <TextField
-                                        error={false}
-                                        id={'register-name'}
-                                        label={'Your Name'}
-                                        value={name}
-                                        onChange={e => setName(e.target.value)}
-
-                                    />
-                                </div>
-                                <div id={"register-surname-container"} className={props.mode}>
-                                    <TextField
-                                        error={false}
-                                        id={'register-surname'}
-                                        label={'Your Surname'}
-                                        value={surname}
-                                        onChange={e => setSurname(e.target.value)}
-
-                                    />
-                                </div>
-                            </div>
-
-                            <div id={"register-email-container"} className={props.mode}>
-                                <TextField
-                                    error={false}
-                                    id={'register-email'}
-                                    label={'Your E-mail'}
-                                    value={email}
-                                    onChange={e => setEmail(e.target.value)}
-
-                                />
-                            </div>
-                            <div id='register-password-container' className={props.mode}>
-                                <TextField
-                                    type='register-password'
-                                    error={false}
-                                    id='password'
-                                    label="Password"
-                                    value={password}
-                                    onChange={e => setPassword(e.target.value)}
-                                />
-                            </div>
-                            <div id='register-repassword-container' className={props.mode}>
-                                <TextField
-                                    type='password'
-                                    error={false}
-                                    id='register-repassword'
-                                    label="Password again"
-                                    value={rePassword}
-                                    onChange={e => setRePassword(e.target.value)}
-                                />
-                            </div>
-                            <div id={'button-container'} className={props.mode}>
-                                <Button
-                                    id={'registerButton'}
-                                    className={props.mode}
-                                    variant="contained"
-                                    onClick={registerHandler}
-                                >
-                                    <b>Kayıt ol</b>
-                                </Button>
-                            </div>
-
-                        </FormGroup>
-                    </FormControl>
+  return (
+    <div id="register-page" className={props.mode}>
+      <div id={"register-page-general"} className={props.mode}>
+        <div id={"register-container"} className={props.mode}>
+          <FormControl noValidate autoComplete={"off"}>
+            <FormGroup row={false}>
+              <div id={"name-surname-container"} className={props.mode}>
+                <div id={"register-name-container"} className={props.mode}>
+                  <TextField
+                    error={false}
+                    id={"register-name"}
+                    label={"Your Name"}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
+                <div id={"register-surname-container"} className={props.mode}>
+                  <TextField
+                    error={false}
+                    id={"register-surname"}
+                    label={"Your Surname"}
+                    value={surname}
+                    onChange={(e) => setSurname(e.target.value)}
+                  />
+                </div>
+              </div>
 
-            </div>
+              <div id={"register-email-container"} className={props.mode}>
+                <TextField
+                  error={false}
+                  id={"register-email"}
+                  label={"Your E-mail"}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div id="register-password-container" className={props.mode}>
+                <TextField
+                  type="register-password"
+                  error={false}
+                  id="password"
+                  label="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div id="register-repassword-container" className={props.mode}>
+                <TextField
+                  type="password"
+                  error={false}
+                  id="register-repassword"
+                  label="Password again"
+                  value={rePassword}
+                  onChange={(e) => setRePassword(e.target.value)}
+                />
+              </div>
+              <div id={"button-container"} className={props.mode}>
+                <Button
+                  id={"registerButton"}
+                  className={props.mode}
+                  variant="contained"
+                  onClick={registerHandler}
+                >
+                  <b>Kayıt ol</b>
+                </Button>
+              </div>
+            </FormGroup>
+          </FormControl>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-const mapStateToProps = ({applicationReducer}) => {
-    const {mode} = applicationReducer;
-    return {
-        mode,
-    };
+const mapStateToProps = ({ applicationReducer }) => {
+  const { mode } = applicationReducer;
+  return {
+    mode,
+  };
 };
 export default connect(mapStateToProps)(Register);
