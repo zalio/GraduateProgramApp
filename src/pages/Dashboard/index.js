9395f4230adfe2c352cede65754fa631d28ba141
zalio <orulluoglu.zeynel@gmail.com>
2020-05-20 18:30:50 +0300

    header is added

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,52 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Button } from "@material-ui/core";
-import { useHistory } from "react-router-dom";
-
-import {
-  registerRequest,
-  registerSuccess,
-  clearUserData,
-} from "../../store/actions/auth";
-import { SESSION_STORAGE_KEY } from "../Login";
-
 import "./dashboard.scss";
 
-const Dashboard = ({ clearUserData }) => {
-  const history = useHistory();
-
-  const onExitPress = async () => {
-    localStorage.removeItem(SESSION_STORAGE_KEY);
-    clearUserData();
-    history.push("/");
-  };
-
+const Dashboard = () => {
   return (
     <>
       <div>Welcome to dashboard</div>
-      <Button
-        style={{
-          marginTop: 50,
-          backgroundColor: "red",
-        }}
-        variant="contained"
-        onClick={onExitPress}
-      >
-        <b style={{ color: "#FFFFFF" }}> EXIT</b>
-      </Button>
     </>
   );
 };
 
-const mapStateToProps = ({ authReducer }) => {
-  const { loading } = authReducer;
-  return { loading };
-};
-
-const mapDispatchToProps = {
-  registerRequest,
-  registerSuccess,
-  clearUserData,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default Dashboard;
