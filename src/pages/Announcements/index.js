1afbaf0e395d910f518afb45c7e3a6c4ce314223
zalio <orulluoglu.zeynel@gmail.com>
2020-05-22 01:13:31 +0300

    announcements and notifications pages is added

--- src/pages/Announcements/index.js
+++ src/pages/Announcements/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
+import CustomCard from "../../components/reusable/CustomCard";
+import Container from "@material-ui/core/Container";
+import "./announcements.scss";
 
-const Announcements = () => {
-  return <div>nabıyon anno</div>;
+const Announcements = ({ mode }) => {
+  return (
+    <div id="cards-page">
+      <Container>
+        <div id="cards-page-upper-text" className={mode}>
+          <h1>
+            <b>Announcements</b>
+          </h1>
+        </div>
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+      </Container>
+    </div>
+  );
 };
-
-export default Announcements;
+const mapStateToProps = ({ applicationReducer }) => {
+  const { mode } = applicationReducer;
+  return {
+    mode,
+  };
+};
+export default connect(mapStateToProps)(Announcements);
