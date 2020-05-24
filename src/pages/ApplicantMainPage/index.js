e4c953df2ab0c00378d0fca7def8cff137ed9e5d
zalio <orulluoglu.zeynel@gmail.com>
2020-05-25 02:13:34 +0300

    firebase connections to announcements  and notifications

--- src/pages/ApplicantMainPage/index.js
+++ src/pages/ApplicantMainPage/index.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Notifications from "../../components/reusable/Notifications";
 import Announcements from "../../components/reusable/Announcements";
 
-const ApplicantMainPage = ({ mode }) => {
+const ApplicantMainPage = ({ mode, announcements, notifications }) => {
   return (
     <>
       <div id="dashboard" className={mode}>
         <Container className={mode}>
           <Grid container spacing={3}>
             <Grid item xs={7}>
-              <Announcements />
+              <Announcements
+                title="Announcements"
+                type="announcements"
+                data={announcements}
+                route="announcements"
+              />
             </Grid>
             <Grid item xs={5}>
-              <Notifications />
+              <Notifications
+                title="Notifications"
+                type="notifications"
+                data={notifications}
+                route="notifications"
+              />
             </Grid>
           </Grid>
         </Container>
@@ -25,9 +35,11 @@ const ApplicantMainPage = ({ mode }) => {
 };
 
 const mapStateToProps = ({ applicationReducer }) => {
-  const { mode } = applicationReducer;
+  const { mode, announcements, notifications } = applicationReducer;
   return {
     mode,
+    announcements,
+    notifications,
   };
 };
 export default connect(mapStateToProps)(ApplicantMainPage);
